8d9401a77600f7a39f7c9103ffe1ebbd12a76e1c
Thomás Kuhn <thomas.kuhn@thomask-ltm4wrw.internal.salesforce.com>
2023-06-14 16:11:08 -0700

    layout basically done

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -1,19 +1,58 @@
 import * as React from "react"
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from "../Navbar/Navbar"
 import Sidebar from "../Sidebar/Sidebar"
 import Home from "../Home/Home"
 import "./App.css"
 
+import SubNav from "../SubNav/SubNav"
+
+
+//not part of project
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
+
 export default function App() {
+
+  let url = 'https://codepath-store-api.herokuapp.com/store'
+
+  const[products, setProducts] = useState([]);
+
+  const [searchValue, setSearchValue] = useState('');
+
+
+  useEffect(() => {
+    axios.get(url)
+    .then((response)=> {     
+      setProducts(response.data.products)
+      
+    })
+  }, [] );
+
+
   return (
     <div className="app">
       <BrowserRouter>
+
+      {/* <Routes>
+
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        
+      </Routes> */}
+
+
         <main>
           {/* YOUR CODE HERE! */}
           <Navbar />
-          <Sidebar />
-          <Home />
+          {/* <Sidebar /> */}
+          <div class='title'> 
+            <h1>Welcome to my store!</h1>
+            <p>We have all kinds of click on the items and start exploring</p>
+          </div>
+          <SubNav searchValue={[searchValue, setSearchValue]}/>
+          <Home products={products}/>
         </main>
       </BrowserRouter>
     </div>
